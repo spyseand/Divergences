4fc09e7690a8c95f42acdcd4dc88e0a1b26fa6b9
Melogale <zachariabadgers@gmail.com>
2021-02-18 06:10:07 -0500

    states!

---
+++ history/states/createStates.bat
@@ -0,0 +1,28 @@
+SET /A i = 1204 
+:loop 
+
+IF %i%==1265 GOTO END 
+.>%i%.txt
+SET /A j = %i% - 1000
+(
+  echo state={
+  echo      id=%j%
+  echo      name="STATE_%i%"
+  echo      manpower = 1
+  echo      state_category = town
+  echo      history = {
+  echo          buildings = {
+  echo              infrastructure = 1
+  echo              arms_factory = 1
+  echo              industrial_complex = 1
+  echo          }
+  echo      }
+  echo      provinces = {
+  echo          PROVINCES
+  echo      }
+  echo  }
+) > %i%.txt
+SET /a i=%i%+1 
+GOTO :LOOP 
+:END
+PAUSE
